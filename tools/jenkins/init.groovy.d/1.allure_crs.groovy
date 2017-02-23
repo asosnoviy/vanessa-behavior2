89cc2b49671b0308cb9d4539a6557920a4738ece
Сосна Евгений <shenja@sosna.zp.ua>
2017-02-23 11:50:59 +0300

    Добавил для плагина branch-api ограничение
    на длину папки, для каждого pr или ветки.

--- tools/jenkins/init.groovy.d/1.allure_crs.groovy
+++ tools/jenkins/init.groovy.d/1.allure_crs.groovy
@@ -4,3 +4,4 @@ import hudson.*
 import hudson.model.*
 
 System.setProperty(hudson.model.DirectoryBrowserSupport.class.getName() + ".CSP", "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';")
+jenkins.branch.WorkspaceLocatorImpl.PATH_MAX = 8;
